b4aac029132cee1d2c7354c7651ef949747ba689
Wenzhong Zheng <Andrew198712@gmail.com>
2019-12-06 11:24:57 -0500

    Initial Commit

---
+++ CoordinatorBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorBase
+//
+//  Created by wenzhong zheng on 2019-12-06.
+//  Copyright © 2019 wenzhong zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
